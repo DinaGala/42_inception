366a0166e9af38b02277c28e407ffc206f71b7e1
Nadezhda Zhuzhleva <nzhuzhle@car11s6.42barcelona.com>
2024-10-20 18:45:45 +0200

    changed js script

--- srcs/requirements/bonus/static/tools/server.js
+++ srcs/requirements/bonus/static/tools/server.js
@@ -1,6 +1,12 @@
 
 import express from 'express'; // Change this to import syntax
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Define __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 const port = 3000;
