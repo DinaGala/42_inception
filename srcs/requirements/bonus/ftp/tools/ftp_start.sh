db64b9140c7c4e28b64123f6f5eb326c3f298e13
Dina Zhuzhleva <dina.zhuzhleva@gmail.com>
2024-10-22 10:42:14 -0400

    changed ftp config

--- srcs/requirements/bonus/ftp/tools/ftp_start.sh
+++ srcs/requirements/bonus/ftp/tools/ftp_start.sh
@@ -11,15 +11,17 @@ if ! id "${FTP_USR}" &>/dev/null; then
     chmod 777 /var/www/html/ftp
 fi
 
+sed -i -r "s/listen=NO/listen=YES/1"   /etc/vsftpd.conf
 sed -i -r "s/#write_enable=YES/write_enable=YES/1"   /etc/vsftpd.conf
 sed -i -r "s/#chroot_local_user=YES/chroot_local_user=YES/1"   /etc/vsftpd.conf
 echo "
 local_enable=YES
+listen_port=21
 allow_writeable_chroot=YES
 pasv_enable=YES
 local_root=/var/www/html/ftp
-pasv_min_port=40000
-pasv_max_port=40005
+pasv_min_port=21100
+pasv_max_port=21110
 userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd.conf
 
 service vsftpd stop
